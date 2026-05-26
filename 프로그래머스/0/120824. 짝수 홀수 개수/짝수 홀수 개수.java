69f659d82cdf17f410fb81b3770ba6a22d72af79
Yerin Lee <91695537+yxinot@users.noreply.github.com>
2026-05-26 20:07:10 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        int odd = 0;
+        int even = 0;
+        for(int n : num_list){
+            if(n % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+        answer[0] = even;
+        answer[1] = odd;
+        return answer;
+    }
+}
