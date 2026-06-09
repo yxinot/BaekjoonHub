f2455e5cdafa08d51bb1331f4c3a25446ab8f017
Yerin Lee <91695537+yxinot@users.noreply.github.com>
2026-06-09 21:29:38 +0900

    [level 2] Title: [1차] 캐시, Time: 25.16 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,33 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        HashMap<String, Integer> cache = new HashMap<>();
-        Queue<String> queue = new LinkedList<>();
-        for(String c : cities){
-            String city = c.toLowerCase();
-            if(cache.containsKey(city)){
-                answer += 1;
-                queue.remove(city);
-                queue.add(city);
-                continue;
-            }
-            else
-                answer += 5;
-            if(cacheSize == 0)
-                continue;
-            if(queue.size() < cacheSize){
-                cache.put(city, 1);
-                queue.add(city);
-            }
-            else{
-                cache.remove(queue.peek());
-                queue.remove();
-                queue.add(city);
-                cache.put(city,1);
-            }
-        }
         return answer;
     }
 }
