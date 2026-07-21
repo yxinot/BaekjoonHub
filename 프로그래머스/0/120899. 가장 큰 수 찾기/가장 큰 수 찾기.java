d01da62461ddbad753202be8d8ab5b5ea52cdab6
Yerin Lee <91695537+yxinot@users.noreply.github.com>
2026-07-21 19:54:10 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int idx = 0;
+        int max = 0;
+        for(int i=0;i<array.length;i++){
+            if(max < array[i]){
+                max = array[i];
+                idx = i;
+            }
+        }
+        answer[0] = max;
+        answer[1] = idx;
+        return answer;
+    }
+}
